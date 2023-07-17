45c578d28ad7cb788eaf3c1ca20bee4c4cf1aed1
aacsolutions-anthony <131961269+aacsolutions-anthony@users.noreply.github.com>
2023-07-17 11:46:28 +1000

    Update vlc_integration.py
    Attempt fix on VLC stream

--- vlc_integration.py
+++ vlc_integration.py
@@ -10,30 +10,33 @@ class VLCPlayer:
         self.process = None
         logging.info("VLCPlayer initiated")
 
-    def _start_process(self, media_path, channel_url):
-        # Verify that media_path exists
-        if not os.path.exists(media_path):
-            logging.error(f"Media path does not exist: {media_path}")
-            return
+def _start_process(self, media_path, channel_url):
+    # Verify that media_path exists
+    if not os.path.exists(media_path):
+        logging.error(f"Media path does not exist: {media_path}")
+        return
 
-        # Split the VLC output string into parts
-        part1 = "#transcode{vcodec=h264,acodec=mpga,ab=128,channels=2,samplerate=44100,scodec=none}:rtp{dst="
-        part2 = ",port=5004,mux=ts}"
-        transcode_options = part1 + channel_url + part2
+    # Split the VLC output string into parts
+    part1 = "#transcode{vcodec=h264,acodec=mpga,ab=128,channels=2,samplerate=44100,scodec=none}:rtp{dst="
+    part2 = ",port=5004,mux=ts}"
+    transcode_options = part1 + channel_url + part2
 
-        # Create the command string
-        cmd = f'cvlc {media_path} --sout "{transcode_options}"'
-        cmd_list = shlex.split(cmd)
-        #Debug print
-        print(cmd_list)
+    # Create the command string
+    cmd = f'cvlc {media_path} --sout "{transcode_options}" --loop'
+    cmd_list = shlex.split(cmd)
+    #Debug print
+    print(f"Built command using the follwing: {cmd_list}")
 
-        # Start the new process
-        try:
-            self.process = subprocess.Popen(cmd_list, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            logging.info("Process started")
-        except Exception as e:
-            logging.error(f"Failed to start process: {e}")
-
+    # Start the new process
+    try:
+        self.process = subprocess.Popen(cmd_list)
+        logging.info("Process started")
+        print("Stream started") 
+        
+    except Exception as e:
+        logging.error(f"Failed to start process: {e}")
+        print(f"Stream failed to start: {e}") 
+    
     def play_media(self, media_path, channel_url):
         # Check if there is an existing process running
         if self.process and self.process.poll() is None:
